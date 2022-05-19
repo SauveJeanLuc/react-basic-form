09b3616081e26dd312d680506fac5e1842c38788
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-05-19 21:45:05 +0200

    form html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,35 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='form-container'>
+      <form className='register-form'>
+        <input 
+          id='first-name' 
+          className='form-field' 
+          type="text" 
+          placeholder='First Name'
+          name ='firstName'
+        />
+        <input 
+          id='last-name' 
+          className='form-field' 
+          type="text" 
+          placeholder='Last Name'
+          name ='lastName'
+        />
+        <input 
+          id='email' 
+          className='form-field' 
+          type='text'
+          placeholder='email'
+          name ='email'
+        />
+        <button className='form-field' type='submit'>
+          Register
+        </button>
+      </form>
     </div>
-  )
+  );
 }
-
-export default App
